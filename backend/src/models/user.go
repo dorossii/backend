1df20cec6f6882a2b99df3e7f9fcb16d709cd59e
mattuu0 <tomatomato170@gmail.com>
2026-05-01 14:16:30 +0900

    add: ユーザー情報を保存するテーブルを追加

---
+++ backend/src/models/user.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type User struct {
+	UserID      string    `json:"UserID"; gorm:"primaryKey"`  // ユーザのID
+	UserName    string    `json:"UserName"`                   // ユーザの名前
+	BirthDate   time.Time `json:"BirthDate"`                  // ユーザの誕生日
+	Mailadress  string    `json:"Mailadress"`                 // ユーザのメールアドレス
+	HealthPoint int       `json:"HealthPoint"; default:"100"` // ユーザの体力
+	DirtLevel   int       `json:"DirtLevel"; default:"0"`     // ユーザの汚れレベル
+	TargetUser  string    `json:"TargetUser"`                 // 嫌がらせ対象のユーザのUserID
+	BgColor     string    `json:"BgColor"; default:"#ffb6c1"` // ユーザの背景色
+}
