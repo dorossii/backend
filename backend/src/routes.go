cbb5994d068b61d38d25ffc95e75b94d6aab9052
yohhhh-yoh <olw.hina0402@gmail.com>
2026-05-01 16:16:51 +0900

    [update]APIのルーティング追記

--- backend/src/routes.go
+++ backend/src/routes.go
@@ -17,9 +17,72 @@ func InitRouter(router *echo.Echo) *echo.Echo {
 	// userグループ
 	user := router.Group("/user")
 	{
+		// 初回ユーザー登録
 		user.POST("/register", TempController)
+
+		// ユーザー情報の編集
+		user.PUT("/setting", TempController)
+
+		// 生活環境情報の登録
+		user.POST("/lifestyle", TempController)
+
+		// 生活環境情報の編集
+		user.PUT("/lifestyle", TempController)
+
+		// タスクグループ
+		task := router.Group("/task")
+		{
+			// タスク取得
+			task.GET("/", TempController)
+
+			// タスク詳細取得
+			task.GET("/:task_id", TempController)
+
+			// タスク写真のアップロード
+			task.POST("/:task_id/image", TempController)
+
+			// タスク煽りメッセージ
+			task.POST("/:task_id/message", TempController)
+
+			// 写真確認
+			task.GET("/:task_id/image", TempController)
+
+			// タスクのステータス更新
+			task.PUT("/:task_id", TempController)
+		}
+
+		// タスク複数完了
+		user.POST("/tasks/complete", TempController)
 	}
 
+	// friendグループ
+	friend := router.Group("/friend")
+	{
+		// フレンド一覧取得
+		friend.GET("/", TempController)
+
+		// フレンド招待
+		friend.GET("/invite", TempController)
+
+		// フレンド認証
+		friend.POST("/accept", TempController)
+
+		// フレンド削除
+		friend.DELETE("/:friend_id", TempController)
+
+		// 嫌がらせする人の設定
+		friend.PUT("/attack", TempController)
+
+		// レスキューする人の設定
+		friend.POST("/rescue", TempController)
+	}
+
+	// noticeグループ
+	notice := router.Group("/notice")
+	{
+		// 通知取得
+		notice.GET("/", TempController)
+	}
 
 	return router
 }
