6ec91dab8f4f267cfb10b28a16fde23c34041953
yohhhh-yoh <olw.hina0402@gmail.com>
2026-05-15 14:17:07 +0900

    [update]項目ミスの修正

--- backend/src/models/task.go
+++ backend/src/models/task.go
@@ -13,7 +13,7 @@ const (
 type Task struct {
 	TaskID       string    `json:"TaskID" gorm:"primaryKey"`     // タスクのID
 	UserID       string    `json:"UserID"`                        // タスクを持つユーザのID
-	Status       int       `json:"TaskName"`                      // タスクの名前
+	Status       TaskStatus `json:"Status"`                      	// タスクの状況
 	StartTime    time.Time `json:"StartTime"`                     // タスクが付与された時間
 	EndTime      time.Time `json:"EndTime"`                       // タスクが終了する時間
 	ImageID      string    `json:"ImageID"`                       // 画像のID (タスクの内容を表す画像)
