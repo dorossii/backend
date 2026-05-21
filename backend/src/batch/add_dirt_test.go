bc8a3cb04edc5381df40516c33fa1f19829f7347
minato0129 <gororyo0129@gmail.com>
2026-05-21 13:54:05 +0900

    テストユーザーを入れるコードを削除

--- backend/src/batch/add_dirt_test.go
+++ backend/src/batch/add_dirt_test.go
@@ -4,49 +4,11 @@ import (
 	"backend/batch"
 	"backend/models"
 	"testing"
-	"time"
 )
 
 
 // AddDirt() 単体テスト
 func TestAddDirt(t *testing.T) {
-
-		// テスト用データの準備 (ユーザー2人、ベースタスク3つ)
-	users := []models.User{
-		{
-			UserID:      "user-001",
-			UserName:    "syatyo",
-			BirthDate:   time.Date(2004, 1, 1, 0, 0, 0, 0, time.UTC),
-			Mailadress:  "user1@example.com",
-			HealthPoint: 100,
-			DirtLevel:   0,
-			Combo:       0,
-			BgColor:     "#ffb6c1",
-		},
-		{
-			UserID:      "user-002",
-			UserName:    "goro",
-			BirthDate:   time.Date(2004, 2, 2, 0, 0, 0, 0, time.UTC),
-			Mailadress:  "user2@example.com",
-			HealthPoint: 90,
-			DirtLevel:   10,
-			Combo:       1,
-			BgColor:     "#add8e6",
-		},
-		{
-			UserID:      "user-003",
-			UserName:    "jiro",
-			BirthDate:   time.Date(2004, 3, 3, 0, 0, 0, 0, time.UTC),
-			Mailadress:  "user3@example.com",
-			HealthPoint: 80,
-			DirtLevel:   700,
-			Combo:       2,
-			BgColor:     "#90ee90",
-		},
-	}
-	if err := models.DB.Create(&users).Error; err != nil {
-		t.Fatalf("failed to create dummy users: %v", err)
-	}
 
 	//テスト対象の関数を実行
 	err := batch.AddDirt()
