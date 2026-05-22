8a7a825c99186a74e364f7bd38181f25785987af
minato0129 <gororyo0129@gmail.com>
2026-05-22 15:11:28 +0900

    テストの結果をわかりやすいように変更

--- backend/src/batch/delete_task_test.go
+++ backend/src/batch/delete_task_test.go
@@ -3,6 +3,7 @@ package batch_test
 import (
 	"backend/batch"
 	"backend/models"
+	"log"
 	"testing"
 	"time"
 )
@@ -35,6 +36,16 @@ func TestDeleteTask(t *testing.T) {
 	if err := models.DB.Create(&task2).Error; err != nil {
 		t.Fatalf("Failed to create task2: %v", err)
 	}
+	//テスト実行前のタスクの表示
+	var beforeTasks []models.Task
+	if err := models.DB.Find(&beforeTasks).Error; err != nil {
+		t.Fatalf("Failed to find tasks: %v", err)
+	}	
+	//タスクの表示
+	log.Println("タスクの一覧(実行前):")
+	for _, task := range beforeTasks {
+		log.Printf("- TaskID: %s, UserID: %s, EndTime: %s", task.TaskID, task.UserID, task.EndTime)
+	}
 
 	//テスト対象の関数を実行
 	err := batch.DeleteTask()
@@ -42,22 +53,29 @@ func TestDeleteTask(t *testing.T) {
 		t.Fatalf("Failed to delete tasks: %v", err)
 	}
 
-	//結果の検証
-	var tasks []models.Task
-	err = models.DB.Find(&tasks).Error
+	//削除後のタスクを取得
+	var afterTasks []models.Task
+	if err := models.DB.Find(&afterTasks).Error; err != nil {
+		t.Fatalf("Failed to find tasks: %v", err)
+	}
+	//削除後のタスクの表示
+	log.Println("タスクの一覧(実行後):")
+	for _, task := range afterTasks {
+		log.Printf("- TaskID: %s, UserID: %s", task.TaskID, task.UserID)
+	}
 
 	if err != nil {
 		t.Fatalf("Failed to find tasks: %v", err)
 	}
 
 	//期限切れのタスクが削除されているかを検証
-	if len(tasks) != 1 {
-		t.Fatalf("Unexpected number of tasks: got %d, want 1", len(tasks))
+	if len(afterTasks) != 1 {
+		t.Fatalf("Unexpected number of tasks: got %d, want 1", len(afterTasks))
 	}
 
 	//残っているタスクがtask2であることを検証
-	if tasks[0].TaskID != task2.TaskID {
-		t.Errorf("Unexpected task remaining: got TaskID %s, want TaskID %s", tasks[0].TaskID, task2.TaskID)
+	if afterTasks[0].TaskID != task2.TaskID {
+		t.Errorf("Unexpected task remaining: got TaskID %s, want TaskID %s", afterTasks[0].TaskID, task2.TaskID)
 	}
 }
 
