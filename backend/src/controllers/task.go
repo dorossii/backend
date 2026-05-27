4a10ed4f779fc5a7c58d199aefe5edfd9ce13cc1
yohhhh-yoh <olw.hina0402@gmail.com>
2026-05-27 12:01:43 +0900

    [update]煽りメッセージ登録の処理追加

--- backend/src/controllers/task.go
+++ backend/src/controllers/task.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"backend/services"
+	"net/http"
+	"errors"
 
 	"github.com/labstack/echo/v4"
 )
@@ -24,4 +26,39 @@ func GetTask(ctx echo.Context) error {
 	return ctx.JSON(200, map[string]interface{}{
 		"tasks": tasks,
 	})	
+}
+
+type PostTaskTauntMessageRequest struct {
+	FriendID string `json:"friendId"`
+	Message  string `json:"message"`
+}
+
+// 煽りメッセージの登録
+func PostTauntMessageHandler(ctx echo.Context) error {
+	id := ctx.Request().Header.Get("UserID")
+
+	var req PostTaskTauntMessageRequest
+
+	if err := ctx.Bind(&req); err != nil {
+		return ctx.JSON(http.StatusBadRequest, echo.Map{
+			"error": "invalid request",
+		})
+	}
+
+	err := services.PostTaskTauntMessage(id, req.FriendID, req.Message)
+	if err != nil {
+		if errors.Is(err, services.ErrFriendNotFound) {
+			return ctx.JSON(http.StatusForbidden, echo.Map{
+				"error": "friend not found",
+			})
+		}
+
+		return ctx.JSON(http.StatusInternalServerError, echo.Map{
+			"error": "internal server error",
+		})
+	}
+
+	return ctx.JSON(http.StatusOK, echo.Map{
+		"message": "success",
+	})
 }
--- backend/src/services/task.go
+++ backend/src/services/task.go
@@ -2,8 +2,17 @@ package services
 
 import (
 	"backend/logger"
+	"backend/models"
 	"backend/repositories"
+
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
 )
+
+// errors
+var ErrFriendNotFound = errors.New("指定されたフレンドが存在しません")
 
 func GetTasks(userID string) ([]repositories.TaskResponse, error) {
 	tasks, err := repositories.GetUserTasks(userID)
@@ -12,4 +21,34 @@ func GetTasks(userID string) ([]repositories.TaskResponse, error) {
 		return []repositories.TaskResponse{}, err
 	}
 	return tasks, nil
+}
+
+// 煽りメッセージの登録
+func PostTaskTauntMessage(userId string, friendId string, msg string) error {
+	// フレンド存在確認
+	friendShip, err := repositories.GetFriendShipAny(userId, friendId)
+	if err != nil {
+		return err
+	}
+
+	if friendShip == nil {
+		return ErrFriendNotFound
+	}
+
+	// メッセージの登録
+	notice := &models.RemindNotice{
+		NoticeID:   uuid.NewString(),
+		UserID:     userId,
+		SenderID:   friendId,
+		Title:      msg,
+		NotifiedAt: time.Now(),
+	}
+
+	err = repositories.CreateRemindNotiec(notice)
+	if err != nil {
+		logger.PrintErr("create remind notice", err)
+		return err
+	}
+
+	return nil
 }
