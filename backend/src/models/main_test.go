f8532d95b99a884aa7119893d679ca39c34706f9
mattuu0 <tomatomato170@gmail.com>
2026-06-03 11:42:55 +0900

    わざとテストを失敗するように変更

--- backend/src/models/main_test.go
+++ backend/src/models/main_test.go
@@ -1,12 +1,14 @@
 package models_test
 
 import (
-	"backend/models"
+	// "backend/models"
 	"os"
 	"testing"
 )
 
 func TestMain(m *testing.M) {
-	models.InitForTest()
-	os.Exit(m.Run())
+	// models.InitForTest()
+	// os.Exit(m.Run())
+	// わざと失敗するように変更
+	os.Exit(1)
 }
