6a26a6cb0e5f31d443c3007514577e090e2531d5
minato0129 <gororyo0129@gmail.com>
2026-06-10 09:30:43 +0900

    既存のレスキューをuseridを元に削除する

--- backend/src/repositories/rescue.go
+++ backend/src/repositories/rescue.go
@@ -4,7 +4,12 @@ import (
 	"backend/models"
 )
 
-// UpdateRescuerSettings はレスキューセッティングを更新する
+// レスキューを更新する
 func UpdateRescuerSettings(userID, targetUser string) error {
-	return models.DB.Model(&models.User{}).Where("UserID = ?", userID).Update("FriendID", targetUser).Error
+	return models.DB.Model(&models.HelpTargets{}).Where("user_id = ?", userID).Create(&models.HelpTargets{UserID: userID, FriendID: targetUser}).Error
+}
+
+// レスキューを削除する
+func DeleteRescuerSettings(userID string) error {
+	return models.DB.Where("user_id = ? ", userID).Delete(&models.HelpTargets{}).Error
 }
