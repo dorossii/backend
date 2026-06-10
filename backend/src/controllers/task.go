26dc70dbb1893d8c31050d93e1923f1f21c63a2b
yohhhh-yoh <olw.hina0402@gmail.com>
2026-06-10 10:33:36 +0900

    [update]エラーの振り分け追加

--- backend/src/controllers/task.go
+++ backend/src/controllers/task.go
@@ -1,9 +1,10 @@
 package controllers
 
 import (
+	// "backend/logger"
 	"backend/services"
-	"net/http"
 	"errors"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
@@ -25,7 +26,7 @@ func GetTask(ctx echo.Context) error {
 
 	return ctx.JSON(200, map[string]interface{}{
 		"tasks": tasks,
-	})	
+	})
 }
 
 type PostTaskTauntMessageRequest struct {
@@ -64,8 +65,8 @@ func PostTauntMessageHandler(ctx echo.Context) error {
 }
 
 type PutTaskStatusRequest struct {
-	Status  string `json:"Status"`
-	Message string `json:"Message"`
+	Status  string `json:"status"`
+	Message string `json:"message"`
 }
 
 // タスクステータス更新(完了•未完了)
@@ -81,9 +82,40 @@ func PutTaskStatusHandler(ctx echo.Context) error {
 		})
 	}
 
+	// TODO:ここのエラーハンドリング修正の余地あり（ケースで分けてるのが多すぎる気がするから）
 	result, err := services.PutTaskStatus(id, taskID, req.Status, req.Message)
 	if err != nil {
-		return err
+		switch {
+		case errors.Is(err, services.ErrInvalidTaskStatus):
+			return ctx.JSON(http.StatusBadRequest, echo.Map{
+				"error": "invalid task status",
+			})
+
+		case errors.Is(err, services.ErrInvalidRequest):
+			return ctx.JSON(http.StatusBadRequest, echo.Map{
+				"error": "invalid request",
+			})
+
+		case errors.Is(err, services.ErrTaskNotFound):
+			return ctx.JSON(http.StatusNotFound, echo.Map{
+				"error": "task not found",
+			})
+
+		case errors.Is(err, services.ErrTaskExpired):
+			return ctx.JSON(http.StatusConflict, echo.Map{
+				"error": "task expired",
+			})
+
+		case errors.Is(err, services.ErrTaskStatusAlreadyUpdated):
+			return ctx.JSON(http.StatusConflict, echo.Map{
+				"error": "task status already updated",
+			})
+
+		default:
+			return ctx.JSON(http.StatusInternalServerError, echo.Map{
+				"error": "internal server error",
+			})
+		}
 	}
 
 	return ctx.JSON(http.StatusOK, echo.Map{
