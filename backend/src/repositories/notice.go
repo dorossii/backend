37e819976a575e81f429408a08b7457c8e38240e
yohhhh-yoh <olw.hina0402@gmail.com>
2026-06-12 14:01:02 +0900

    [update]汚した相手に通知の登録処理の追加

--- backend/src/repositories/notice.go
+++ backend/src/repositories/notice.go
@@ -7,3 +7,7 @@ import (
 func CreateRemindNotiec(notice *models.RemindNotice) error {
 	return models.DB.Create(notice).Error
 }
+
+func CreateTrashNotice(notice *models.TrashNotice) error {
+	return models.DB.Create(notice).Error
+}
